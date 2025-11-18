4ed01f9a1c6778b8abce7a5cc77048ba044fa598
deboral6 <deboralemos997130537@gmail.com>
2025-11-18 18:58:57 +0000

    lapiseira

--- myrep/poo/lapiseira/py/draft.py
+++ myrep/poo/lapiseira/py/draft.py
@@ -14,22 +14,25 @@ class Grafite:
         self.tam -= valor
         if self.tam < 0:
             self.tam = 0
+
     def usageParsheet(self):
         return 4
+
     def __str__(self):
-        return f"{self.__calibre}: {self.__dureza}: {self.tam}"
+        return f"{self.__calibre}:{self.__dureza}:{self.tam}"
         
+
 class Lapiseira:
-    def __init__(self, calibre: float, bico: float):
-        self.__calibre: float
-        self.bico = float
-        self.tambor: list[Grafite | None] = []
+    def __init__(self, calibre: float):
+        self.__calibre = calibre
+        self.__bico: Grafite | None = None
+        self.tambor: list[Grafite] = []
 
     def getCalibre(self) -> float:
         return self.__calibre
     
     def getBico(self) -> Grafite | None:
-        return self.bico
+        return self.__bico
     
     def getTambor(self) -> list[Grafite]:
         return self.tambor
@@ -38,21 +41,25 @@ class Lapiseira:
         if graf.getCalibre() == self.__calibre:
             self.tambor.append(graf)
         else:
-            print("fail: grafite incompativel")
+            print("fail: calibre incompatível")
 
     def puxarGrafite(self):
         if self.__bico is not None:
             print("fail: ja existe grafite no bico")
+            return
 
         if len(self.tambor) == 0:
             print("fail: tambor vazio")
             return
+        
         self.__bico = self.tambor.pop(0)
 
     def removerGrafite(self):
-        if self.bico is None:
-            print("fail: não existe grafite no bico")
-        self.bico = None
+        if self.__bico is None:
+            print("fail: nao existe grafite no bico")
+            return
+
+        self.__bico = None
 
     def escrever(self):
         if self.__bico is None:
@@ -62,6 +69,7 @@ class Lapiseira:
         if self.__bico.getTam() <= 10:
             print("fail: tamanho insuficiente")
             return
+        
         gasto = self.__bico.usageParsheet()
 
         if self.__bico.getTam() - gasto < 10:
@@ -72,8 +80,10 @@ class Lapiseira:
         self.__bico.gastar(gasto)
 
     def __str__(self):
-        return f"calibre{self.__calibre}"
-    
+        bico_str = f"[{self.__bico}]" if self.__bico else "[]"
+        tambor_str = "<" + "".join(f"[{g}]" for g in self.tambor) + ">" if self.tambor else "<>"
+        return f"calibre: {self.__calibre}, bico: {bico_str}, tambor: {tambor_str}"
+
 def main():
     lapiseira = None
     while True:
@@ -83,25 +93,19 @@ def main():
 
         if args[0] == "end":
             break
+        
         elif args[0] == "init":
             lapiseira = Lapiseira(float(args[1]))
-            continue
+
         elif args[0] == "show":
-            calibre = lapiseira.getCalibre()
-            bico = lapiseira.getBico()
-            tambor = lapiseira.getTambor()
+            print(lapiseira)
 
-            bico_str = str(bico) if bico is not None else "[]"
-            tambor_str = "<" + "".join(str(g) for g in tambor) + ">" if len(tambor) else "<>"
-
-            print(f"calibre: {calibre}, bico: {bico_str}, tambor: {tambor_str}")
-        
         elif args[0] == "insert":
-                calibre = float(args[1])
-                dureza = args[2]
-                tam = int(args[3])
-                graf = Grafite(dureza, calibre, tam)
-                lapiseira.insert(graf)
+            calibre = float(args[1])
+            dureza = args[2]
+            tam = int(args[3])
+            graf = Grafite(calibre, dureza, tam)
+            lapiseira.insert(graf)
 
         elif args[0] == "pull":
             lapiseira.puxarGrafite()
@@ -109,35 +113,7 @@ def main():
         elif args[0] == "remove":
             lapiseira.removerGrafite()
 
-        elif args[0] == "white":
+        elif args[0] == "write":
             lapiseira.escrever()
-        elif args[0] == "show":
-            print(lapiseira)
+
 main()
-
-
-
-
-
-        
-
-        
-
-
-
-
-        
-
-        
-    
-
-
-    
-    
-
-
-        
-
-
-        
-        
